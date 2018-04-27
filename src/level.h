89e290f98d654088b7deb97a8220afed10bc88a4
shinespeciall <648616375@qq.com>
2018-04-27 18:52:52 +0800

    add more detail for class level

--- src/level.h
+++ src/level.h
@@ -1,6 +1,10 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
+#ifndef _DEQUE_
+#include <deque>
+#endif
+
 struct LevelHeader
 {
     unsigned char HeaderPointerIndex;   //multiply 4 make a shift for some base pointers
@@ -17,12 +21,35 @@ struct LevelHeader
     unsigned char SHardModeSecondOnePlaceNum;
 };
 
+struct MAPLinkerLineRecord   //only
+{
+    unsigned char LinkerTypeFlag; //x01 for portal only, x02 for instant shift, x03 for door and tube, x04 unknown, x05 unknown
+    unsigned char RoomID; //start from x00
+    unsigned char x1;
+    unsigned char x2;
+    unsigned char y1;
+    unsigned char y2;  //topleft judge block position (x1, y1), bottomleft judge block position (x2, y2), the first block start from (0, 0)
+    unsigned char HorizontalDisplacement;
+    unsigned char VerticalDisplacement;  //there is a position shifting after wario switch to new room
+    unsigned char SpritesMAP_ID;
+    unsigned char BGM_ID_FirstByte;  //Low Byte
+    unsigned char BGM_ID_SecondByte;  //High Byte
+};
+
 class Level
 {
 public:
     Level();
 private:
     struct LevelHeader;
+public:
+    class MAPLinker
+    {
+    public:
+        MAPLinker();
+    private:
+        deque<MAPLinkerLineRecord> MAPLinkerRecord;
+    };
 
 };
 
